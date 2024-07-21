b72bda67b2994dd830e2f6ead770f5737ff2a692
xdaem9n <8muhammadsaleh@gmail.com>
2024-07-21 11:55:35 -0400

    modified the widge_test to resolve workflow error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,11 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-
+import 'package:momnah_portfolio/main.dart'; // This should be correct if your package name is momnah_portfolio
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp() as Widget);
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -27,8 +19,4 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
-}
-
-class MyApp {
-  const MyApp();
 }
