7c72989137a4d4427699879b9b1b698b9a5e0e03
xdaem9n <8muhammadsaleh@gmail.com>
2024-07-21 12:23:24 -0400

    fgh

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,21 +3,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:momnah_portfolio/main.dart'; // Ensure this path is correct
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('HomePage content test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
     // Verify that the app initially displays the expected content.
     expect(find.text('ORGANIZE YOUR NOTES WITH EASE'), findsOneWidget);
 
-    // Check for the theme toggle button
+    // Check for the initial theme toggle button (should be Icons.brightness_6)
     expect(find.byIcon(Icons.brightness_6), findsOneWidget);
 
     // Tap the theme toggle button and trigger a frame.
     await tester.tap(find.byIcon(Icons.brightness_6));
-    await tester.pump();
+    await tester.pumpAndSettle();
 
-    // Verify that the theme has toggled.
-    expect(find.byIcon(Icons.brightness_7), findsOneWidget);
+    // Verify that the theme has toggled (should now be Icons.brightness_4)
+    expect(find.byIcon(Icons.brightness_4), findsOneWidget);
   });
 }
